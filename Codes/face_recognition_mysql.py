48409506b7e4ec5f8a588ec11dd4d936b2cfbbbb
hamed <izadi.hamed1989@gmail.com>
2022-05-16 15:13:57 +0430

    update

--- Codes/face_recognition_mysql.py
+++ Codes/face_recognition_mysql.py
@@ -5,6 +5,8 @@ from PIL import Image
 from datetime import datetime
 
 
+
+
 cnx = mysql.connector.connect(user='root', password='',
                               host='127.0.0.1',
                               database='face_recognition_mapsa')
@@ -20,6 +22,8 @@ face_recognition_model = dlib.face_recognition_model_v1(
 )
 TOLERANCE = 0.6
 
+moment_person = []
+
 def get_face_encodings(frame):
     image = np.asarray(frame)
     
@@ -34,7 +38,7 @@ def compare_face_encodings(known_encoded, face):
 
 def find_match(known_encoded, frame_encoded):
 	matches = compare_face_encodings(known_encoded, frame_encoded)
-
+	
 	if not any(matches):
 		cursor.execute(
 			'INSERT INTO webcam(encoded_image, repeated_number) VALUES (\'%s\',\'%i\')' %(str(frame_encoded.tolist()), 0)
@@ -58,19 +62,53 @@ def encode_mysql(frame):
 	frame_encoded = get_face_encodings(frame)
 	face_encoded_retrieve = []
 	
-	if len(frame_encoded) == 0:
-		return
-	else:
-		cursor.execute('SELECT encoded_image FROM webcam;')
+	global moment_person
+	
+	cursor.execute('SELECT encoded_image FROM webcam;')
 
-		for item in cursor:
-			face_encoded_retrieve.append(np.asarray(eval(item[0])))
-		
+	for item in cursor:
+		face_encoded_retrieve.append(np.asarray(eval(item[0])))
+
+	if len(frame_encoded) == 0:
+		moment_person = []
+		return
+	
+	else:	
 		if not face_encoded_retrieve:
-			cursor.execute(
-				'INSERT INTO webcam (encoded_image, repeated_number) VALUES (\'%s\',\'%i\')' %(str(frame_encoded[0].tolist()), 0)
-			)
-			cnx.commit()
-	for i in range(len(frame_encoded)):
-		find_match(face_encoded_retrieve, frame_encoded[i])
+			for iter in range(len(frame_encoded)):
+				cursor.execute(
+					'INSERT INTO webcam (encoded_image, repeated_number) VALUES (\'%s\',\'%i\')' %(str(frame_encoded[iter].tolist()), 0)
+				)
+				cnx.commit()
+				moment_person.append(frame_encoded[iter])
+			return
+		
+		if not moment_person:
+			moment_person = frame_encoded.copy()
+			for iter in range(len(frame_encoded)):
+				find_match (face_encoded_retrieve, frame_encoded[iter])
+		else:
+			temp = []
+			for iter in range(len(frame_encoded)):
+				matches = compare_face_encodings(moment_person, frame_encoded[iter])
+				temp.append(frame_encoded[iter])
+				if not any(matches):
+					find_match(face_encoded_retrieve, frame_encoded[iter])
+	
+				# else:			
+				# 	count = 0
+				# 	for match in matches:
+				# 		if match:
+				# 			temp.append(moment_person[count])
+				# 			# return
+				# 		count += 1
+			moment_person = temp.copy()
+			
+			# for iter in range(len(moment_person)):
+			# 	if moment_person[iter] in frame_encoded:
+			# 		temp.append(moment_person[iter])
+			# moment_person = temp.copy()
+
+			# moment_person = temp.copy()
+			# moment_person = list(set(temp) & set(moment_person))
 
